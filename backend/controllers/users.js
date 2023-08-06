270c97ef79a45fdb125de5f6e747a8614adeb3d4
Evgenia Fedotova <fedotovaevgenia1793@gmail.com>
2023-08-06 20:25:47 +0400

    "комит"

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -25,7 +25,10 @@ const createUser = (req, res, next) => {
     }))
     .then((user) => {
       res.status(201).send({
-        email, name, about, avatar,
+        email: user.email,
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
       });
     })
     .catch((err) => {

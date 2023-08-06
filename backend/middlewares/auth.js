28a91af470f5315e90c2715c22a6e472ca115015
Evgenia Fedotova <fedotovaevgenia1793@gmail.com>
2023-08-06 20:23:32 +0400

    'комит'

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,8 +1,6 @@
 const jwt = require('jsonwebtoken');
 const ValidationError = require('../errors/ValidationError');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
@@ -15,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
     throw new ValidationError('Необходима авторизация');
   }

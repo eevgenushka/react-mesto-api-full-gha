4562064f725e8f0486d7b226ca074c8516fb425a
Evgenia Fedotova <fedotovaevgenia1793@gmail.com>
2023-08-07 21:47:50 +0400

    "комит"

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const NotFoundError = require('./errors/NotFoundError');
 const { createUser, login } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(cors({
   origin: [
-    'http://localhost:3000'],
+    'http://localhost:3001'],
 }));
 
 app.use(requestLogger);

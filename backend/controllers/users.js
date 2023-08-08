860250c739161ef6ae5504376ea96db031d51575
Evgenia Fedotova <fedotovaevgenia1793@gmail.com>
2023-08-08 20:49:55 +0400

    "комит"

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -49,7 +49,7 @@ const login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret', {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', {
         expiresIn: '7d',
       });
       res.send({ message: 'Авторизация прошла успешно', token });
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
     throw new ValidationError('Необходима авторизация');
   }
